0c8c2e63f78885ae3ccb7001ba3e024910ef9af7
Manjunath Yadav <Manjunath Yadav@Manju12Yadav>
2023-04-03 16:18:44 +0530

    added new class

---
+++ src/test/java/com/mavenPractice/KiranFoods.java
@@ -0,0 +1,17 @@
+package com.mavenPractice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class KiranFoods 
+{
+	@Test
+	public void menuList()
+	{
+		
+		Reporter.log("Masala Dosa",true);
+		System.out.println("Mushroom");
+		
+	}
+	
+}

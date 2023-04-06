944eac495ad598ebfb089df7c82682c7e68d7c13
Manjunath Yadav <Manjunath Yadav@Manju12Yadav>
2023-04-06 16:24:54 +0530

    1st commit

--- src/test/java/com/mavenPractice/MavenTest.java
+++ src/test/java/com/mavenPractice/MavenTest.java
@@ -8,18 +8,19 @@ public class MavenTest
 	@Test(groups = "smoke")
 	public void testScript1()
 	{
-		Reporter.log("-----Test Case :-1 Executed -----",true);
+		Reporter.log("-----Test Case :-1 Executed Smoke-----",true);
 	}
 	
 	@Test(groups = "regression")
 	public void testScript2()
 	{
-		Reporter.log("-----Test Case :-2 Executed -----",true);
+		Reporter.log("-----Test Case :-2 Executed Regression -----",true);
 	}
 	@Test(groups = "regression")
 	public void testScript3()
 	{
-		Reporter.log("-----Test Case :-3 Executed -----",true);
+		Reporter.log("-----Test Case :-3 Executed Regression -----",true);
+		
 	}
 	
 	

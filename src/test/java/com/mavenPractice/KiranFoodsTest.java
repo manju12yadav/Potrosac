d6ea2563bf43fc151ec6cbe1330171acaf390f57
Manjunath Yadav <Manjunath Yadav@Manju12Yadav>
2023-04-06 13:13:10 +0530

    test

---
+++ src/test/java/com/mavenPractice/KiranFoodsTest.java
@@ -0,0 +1,22 @@
+package com.mavenPractice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class KiranFoodsTest 
+{
+	@Test
+	public void menuList()
+	{
+		
+		Reporter.log("Masala Dosa",true);
+		System.out.println("Mushroom");
+		
+	}
+	@Test
+	public void curryPoint()
+	{
+		System.out.println("Guntur Chicken");
+	}
+	
+}
